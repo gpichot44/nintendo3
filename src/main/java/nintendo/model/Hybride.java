f1b6a8e171478864f433eaeb1fff9c6872ae1f7d
Tim <timothy.labidi@gmail.com>
2026-03-04 17:21:56 +0100

    Ajout des trois classes héritant de console

---
+++ src/main/java/nintendo/model/Hybride.java
@@ -0,0 +1,12 @@
+package nintendo.model;
+
+import java.time.LocalDate;
+
+public class Hybride extends Console{
+
+	public Hybride(String nom,int prix, LocalDate dateSortie) {
+		super(nom,prix,dateSortie);
+		
+	}
+
+}
---
+++ src/main/java/nintendo/model/Portable.java
@@ -0,0 +1,12 @@
+package nintendo.model;
+
+import java.time.LocalDate;
+
+public class Portable extends Console {
+
+	public Portable(String nom,int prix, LocalDate dateSortie) {
+		super(nom,prix,dateSortie);
+		
+	}
+
+}
